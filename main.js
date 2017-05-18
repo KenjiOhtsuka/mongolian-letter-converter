ca5be91b9d3d2bf72bd9d35a4fb00d5e366b898a
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-05-18 10:45:27 +0900

    fix alert

--- main.js
+++ main.js
@@ -86,7 +86,6 @@ function cyrillicToAlphabet() {
     var cyrillicText = cyrillicTextarea.value;
     var length = cyrillicText.length;
     var alphabetText = '';
-    alert(length);
     for (var i = 0; i < length; ++i) {
         var c = cyrillicText[i];
         var lowerC = c.toLowerCase();
